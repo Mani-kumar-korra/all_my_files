36a042f957ece367ae80231d67296389ee8565da
Manikumar <z0412@ad.ziffity.com>
2023-10-04 10:46:13 +0530

    magento-assignment-configurations-1: Have fixed the paths for the grid file

--- Config/Task/Block/Adminhtml/Index.php
+++ Config/Task/Block/Adminhtml/Index.php
@@ -1,10 +1,27 @@
 <?php
 namespace Config\Task\Block\Adminhtml;
 
+use Magento\Config\Model\Config\Source\Yesno;
 use Magento\Framework\View\Element\Template;
+use Magento\Config\Model\Config\Structure;
 
-class index extends Template
+class Index extends Template
 {
+    /**
+     * @var Structure
+     */
+    protected $configStructure;
+    protected $matchKey = [];
+
+    public function __construct(
+        Structure $configStructure,
+        Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->configStructure = $configStructure;
+    }
+
     public function getConfigDump()
     {
         // Read the configuration dump from config.php
@@ -18,7 +35,7 @@ class index extends Template
         return is_array($configDump) ? $configDump : [];
     }
 
-    public function flattenConfigArray($configArray, $parentKey = '', $separator = ': ')
+    public function flattenConfigArray($configArray, $parentKey = '', $separator = '/')
     {
         $result = [];
         foreach ($configArray as $key => $value) {
@@ -31,15 +48,54 @@ class index extends Template
 
                 // Skip rows with 'module' in the parent key
                 if ($initialKey !== 'modules') {
+                    // Split the $newKey into parts using the separator
+                    $keyParts = explode($separator, $newKey);
+
+                    // Remove the first two parts (system and default)
+                    $keyParts = array_slice($keyParts, 2);
+
+                    // Rejoin the remaining parts to form the modified key
+                    $newKey = implode($separator, $keyParts);
+
                     $result[] = [
                         'parent_key' => $initialKey,
                         'key' => $newKey,
-                        'value' => $value,
+                        'value' => $value
                     ];
+                    $dummy[$newKey] = $this->getCustomElement($newKey, $initialKey, $value);
                 }
             }
         }
+
+        // Add this line to display the contents of $this->matchKey
+
+
         return $result;
     }
+
+    public function getCustomElement($key, $parent_key, $value)
+    {
+        // Call getElement with the provided key
+        $custom = $this->configStructure->getElement($key);
+        $custom = $custom->getData();
+        if (isset($custom['source_model'])) {
+            // Now, you have the custom element based on the key
+            $isTrue = $custom['source_model'] === Yesno::class ? true : false;
+
+            // Debugging: Display the key when the condition is true
+            if ($isTrue) {
+                $this->matchKey[] = [
+                    'key' => $key,
+                    'parent_key' => $parent_key,
+                    'value' => $value
+                ];
+
+
+            }
+            return $isTrue;
+        }
+        return false;
+    }
+
 
 }
